c0c324f371795833252cd8c66950d332a4731639
Wynne Lo <wynnemlo@gmail.com>
2014-10-07 00:19:11 +0800

    Finished basics.rb

---
+++ basics.rb
@@ -0,0 +1,50 @@
+#Question 1
+puts "Wynne" + " Lo"
+
+#Question 2
+#1)
+puts 4230 / 1000
+#2)
+puts (4230 % 1000) / 100
+#3)
+puts (4230 % 1000 % 100) / 10
+#4)
+puts 4230 % 1000 % 100 % 10
+
+#Question 3
+movies = {
+	back_to_the_future: 1975,
+	just_married: 2004,
+	rift: 2013,
+	final_fantasy: 2001,
+	what_happened: 1981 }
+
+puts movies[:back_to_the_future]
+puts movies[:'just_married']
+puts movies[:rift]
+puts movies[:final_fantasy]
+puts movies[:what_happened]
+
+#Question 4
+array = [1975, 2004, 2013, 2001, 1981]
+
+puts array[0]
+puts array[1]
+puts array[2]
+puts array[3]
+puts array[4]
+
+#Question 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#Question 6
+puts 2.32 * 2.32
+puts 389.3 * 389.3
+puts 23.23 * 23.23
+
+#Question 7
+#The brackets are not equal. A bracket might be left opened, 
+#or an extra closing bracket is present.
